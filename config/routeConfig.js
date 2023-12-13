20c405f5e666ffd91bf23ac7f443379a23c99ce9
koey <thekhoi.k@gmail.com>
2023-12-13 16:34:17 +0700

    first commit

--- config/routeConfig.js
+++ config/routeConfig.js
@@ -1,12 +1,6 @@
 // src/config/routeConfig.js
 const routes = [
-  // { path: '/products', route: require('../routes/productRoute') },
-  // { path: '/categories', route: require('../routes/categoryRoute') },
-  // { path: '/address', route: require('../routes/addressRoute') },
   { path: "/auth", route: require("../src/routes/authRouter") },
-  // { path: '/user', route: require('../routes/userRoute') },
-  // { path: '/orders', route: require('../routes/orderRoute') },
-  // { path: '/cart', route: require('../routes/cartRoute') },
 ];
 
 const configureRoutes = (app) => {
