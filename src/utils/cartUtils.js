8be2628ee999e672afca67897432f3706c96c953
khoi <thekhoi.k@gmail.com>
2024-03-13 16:43:49 +0700

    delete cart when checkout

---
+++ src/utils/cartUtils.js
@@ -0,0 +1,34 @@
+const CartRepository = require("../models/repositories/cartRepository");
+const { verifyProductExistence } = require("./verifyExistence");
+
+class CartUtils {
+  static async remove(cart, foundProduct, foundIndex) {
+    cart.total -= cart.products[foundIndex].quantity * foundProduct.price;
+    cart.products.splice(foundIndex, 1);
+    cart.count_product--;
+    return cart;
+  }
+  static async handleCart(account_id) {
+    const QUERY = {
+      account_id: account_id,
+    };
+    let cart = await CartRepository.findByAccountId(QUERY);
+    if (!cart) {
+      cart = await CartRepository.createCart(account_id);
+    }
+    return cart;
+  }
+  static async removeItem(account_id, product) {
+    let cart = await CartUtils.handleCart(account_id);
+    const foundProduct = await verifyProductExistence(product.product_id);
+    const foundIndex = cart.products.findIndex((el) => el._id === product.product_id);
+    if (foundIndex === -1)
+      throw new NotFoundError(
+        "Product with id: " + product.product_id + " not found in cart"
+      );
+    cart = await this.remove(cart, foundProduct, foundIndex);
+    return await CartRepository.save(cart);
+  }
+}
+
+module.exports = CartUtils;
