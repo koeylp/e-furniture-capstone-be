2041920bc2946d2941369827007fcb24a3ecedf3
khoi <thekhoi.k@gmail.com>
2024-03-20 18:49:01 +0700

    complete mail seding

--- src/utils/mailtrap.js
+++ src/utils/mailtrap.js
@@ -1,23 +1,57 @@
 const { MailtrapClient } = require("mailtrap");
+const ejs = require("ejs");
+const fs = require("fs");
+const ProductRepository = require("../models/repositories/productRepository");
+const { vndFormatCurrency } = require("./format");
 
-const TOKEN = "4ddd80a13ad89cc71116388587f0c656";
+const TOKEN = process.env.MAILTRAP_TOKEN;
 const SENDER_EMAIL = "no-reply@efurniturenotification.live";
-const RECIPIENT_EMAIL = "lethekhoi1919@gmail.com";
+const filePath = "src/templates/emailTemplate.ejs";
+const emailTemplate = fs.readFileSync(filePath, "utf-8");
 
 const client = new MailtrapClient({ token: TOKEN });
 
 const sender = { name: "eFutniture", email: SENDER_EMAIL };
+
+const compiledTemplate = ejs.compile(emailTemplate);
+
 class Mailtrap {
-  static async send() {
-    await client
-      .send({
+  static async send(order) {
+    try {
+      const productsPromises = order.order_products.map(async (product) => {
+        const foundProduct = await ProductRepository.findProductById(
+          product.product_id
+        );
+        return {
+          name: foundProduct.name,
+          thumb: foundProduct.thumbs[0],
+          quantity: product.quantity,
+          price: vndFormatCurrency(product.price),
+        };
+      });
+
+      const products = await Promise.all(productsPromises);
+
+      const data = {
+        order_shipping: order.order_shipping,
+        products: products,
+        total: vndFormatCurrency(order.order_checkout.final_total),
+        order_code: order.order_code,
+      };
+
+      const renderedTemplate = compiledTemplate(data);
+
+      await client.send({
         from: sender,
-        to: [{ email: RECIPIENT_EMAIL }],
+        to: [{ email: order.order_shipping.email }],
         subject: "Order Confirmation!",
-        text: "Welcome to Mailtrap Sending!",
-      })
-      .then(console.log)
-      .catch(console.error);
+        html: renderedTemplate,
+      });
+
+      console.log("Email sent successfully.");
+    } catch (error) {
+      console.error("Error sending email:", error);
+    }
   }
 }
 
