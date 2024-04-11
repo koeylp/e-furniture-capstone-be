6e3d09d84ebdfd1309393a7de51ec451d4459619
khoi <thekhoi.k@gmail.com>
2024-04-11 16:59:08 +0700

    config atlas mongodb

--- src/databases/initMongodb.js
+++ src/databases/initMongodb.js
@@ -1,13 +1,11 @@
 // config/database.js
 const mongoose = require("mongoose");
 const _CONF = require("../config");
-// const { MONGO_USERNAME, MONGO_PASSWORD, MONGO_CLUSTER, MONGO_DB } = process.env;
+const { MONGO_USERNAME, MONGO_PASSWORD, MONGO_CLUSTER } = process.env;
 
 const connectToDatabase = async () => {
   try {
-    // mongo atlas option
-    // const connectionUrl = `mongodb+srv://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_CLUSTER}/${MONGO_DB}?retryWrites=true&w=majority`;
-
+    // const connectionUrl = `mongodb+srv://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_CLUSTER}.zpdt7.mongodb.net/?retryWrites=true&w=majority&appName=${MONGO_CLUSTER}`;
     await mongoose.connect(_CONF.mongo.uri);
     console.log("Connected to the database with URI: " + _CONF.mongo.uri);
   } catch (error) {
