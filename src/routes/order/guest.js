4811d4a2d159fba6c8747a872f3ad0500e1be10b
khoi <thekhoi.k@gmail.com>
2024-04-21 20:50:39 +0700

    paid for guest

--- src/routes/order/guest.js
+++ src/routes/order/guest.js
@@ -4,5 +4,6 @@ const OrderController = require("../../controllers/orderController");
 const { asyncHandler } = require("../../utils/asyncHandler");
 
 router.post("/create-order", asyncHandler(OrderController.createOrderGuest));
+router.get("/payos/:orderCode", asyncHandler(OrderController.payPayOSGuest));
 
 module.exports = router;
