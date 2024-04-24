43d8df7433b2f3c0b1467a6919489aec026c7cf4
TrongToann <nguyentrongtoan9a1nbk@gmail.com>
2024-04-24 14:16:59 +0700

    Update Folder TestCase

--- tests/unit/utils/cart/getCart.test.js
+++ tests/unit/utils/cart/getCart.test.js
@@ -1,5 +1,3 @@
-const { PermissionConstants } = require("../../../../src/utils/roleConstant");
-global.PermissionConstants = PermissionConstants;
 const { describe, it } = require("mocha");
 const sinon = require("sinon");
 const chai = require("chai");
@@ -10,6 +8,7 @@ const CartService = require("../../../../src/services/cartService");
 const { OK } = require("../../../../src/utils/successHandler");
 const CartUtils = require("../../../../src/utils/cartUtils");
 const ProductRepository = require("../../../../src/models/repositories/productRepository");
+const { BadRequestError } = require("../../../../src/utils/errorHanlder");
 
 chai.use(sinonChai);
 const { expect } = chai;
@@ -22,76 +21,67 @@ const mockRes = () => {
 };
 
 describe("CartService", () => {
-  describe("addToCart", () => {
-    it("should add a new product to the cart", async () => {
+  describe("getCart", () => {
+    it("should get list of products from cart", async () => {
       const account_id = "123";
       const product = { _id: "test_id", quantity: 1, variation: "default" };
       const mockProductRepository = sinon
-        .stub(ProductRepository, "checkProductById")
+        .stub(ProductRepository, "findPublishProductByIDWithModify")
         .withArgs(product._id)
         .returns(product);
-      const mockCartUtils = sinon
-        .stub(CartUtils, "handleCart")
-        .withArgs(account_id)
-        .resolves({});
-      const mockCartRepository = sinon
-        .stub(CartRepository, "save")
-        .returns(true);
-      const mockCheckOutOfStock = sinon
-        .stub(CartService, "checkOutOfStock")
-        .returns(false);
 
-      var stub = sinon.stub(CartService, "addToCart");
-      stub.withArgs(account_id, product).returns("100");
-      expect(CartService.addToCart(account_id, product)).to.be.equal("100");
-      expect(ProductRepository.checkProductById(product._id)).to.be.equal(
-        product
-      );
+      var stub = sinon.stub(CartService, "getCart");
+      stub.withArgs(account_id).returns("carts");
+      expect(CartService.getCart(account_id)).to.be.equal("carts");
+      expect(
+        ProductRepository.findPublishProductByIDWithModify(product._id)
+      ).to.be.equal(product);
     });
   });
   it("should throw BadRequestError if data is missing", async () => {
     const req = mockReq({}); // No username or password
+    req.payload = {
+      account_id: null,
+    };
     const res = mockRes();
 
     try {
       await CartController.addToCart(req, res);
     } catch (error) {
+      expect(error).to.be.an.instanceOf(BadRequestError);
+      expect(error.message).to.equal("Bad Request");
       expect(error.message).to.be.Throw;
     }
   });
   it("should throw BadRequestError if account_id is missing", async () => {
-    const product = { _id: "test_id", quantity: 1, variation: "default" };
-    const req = mockReq(product);
+    const req = mockReq();
     const res = mockRes();
-
     try {
-      await CartController.addToCart(req, res);
+      await CartController.getCart(req, res);
     } catch (error) {
       expect(error.message).to.be.Throw;
     }
   });
 
-  it("should call CartService.addToCart and send OK response", async () => {
-    const account_id = "123";
-    const product = { _id: "test_id", quantity: 1, variation: "default" };
-    const req = mockReq(product);
+  it("should call CartService.getCart and send OK response", async () => {
+    const req = mockReq();
     const res = mockRes();
     req.payload = {
       account_id: "123",
     };
 
     const mockCartService = sinon
-      .stub(CartService, "addToCart")
-      .resolves("loginResult");
-    await CartController.addToCart(req, res);
-    expect(mockCartService).to.have.been.calledOnceWith(account_id, product);
+      .stub(CartService, "getCart")
+      .resolves("carts");
+    await CartController.getCart(req, res);
+    expect(mockCartService).to.have.been.calledOnceWith("123");
     expect(res.status).to.have.been.calledWith(200);
     expect(res.json).to.have.been.calledWith(
       new OK({
-        message: "Added to cart",
-        metaData: "loginResult",
+        message: "Your cart",
+        metaData: "carts",
       })
     );
-    CartService.addToCart.restore();
+    CartService.getCart.restore();
   });
 });
